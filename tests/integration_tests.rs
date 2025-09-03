64e30f1557f019d2026e12a52dcfeeecd84f8981
shadr <shadr.nn@gmail.com>
2025-09-03 22:27:28 +0300

    Make tests run much 3 to 4x faster and greatly improve output diff
    * generate test for each input test file
    
    * print diff in failed tests
    
    * change the LF unicode character for ↲ and tab representation for ⇥ and three spaces
    
    ---------
    
    Co-authored-by: Nathan Lovato <12694995+NathanLovato@users.noreply.github.com>

--- tests/integration_tests.rs
+++ tests/integration_tests.rs
@@ -1,12 +1,12 @@
 use gdscript_formatter::format_gdscript;
+use similar::{ChangeTag, TextDiff};
 use std::fs;
 use std::path::Path;
 
 fn make_whitespace_visible(s: &str) -> String {
-    s.lines()
-        .map(|line| line.replace('\t', "→").replace(' ', "·") + "↵")
-        .collect::<Vec<_>>()
-        .join("\n")
+    s.replace(' ', "·")
+        .replace('\t', "⇥   ")
+        .replace('\n', "↲\n")
 }
 
 fn assert_formatted_eq(result: &str, expected: &str, file_path: &Path) {
@@ -15,11 +15,17 @@ fn assert_formatted_eq(result: &str, expected: &str, file_path: &Path) {
             "\nFormatted output doesn't match expected for {}",
             file_path.display()
         );
-        eprintln!("\nEXPECTED:");
-        eprintln!("{}", make_whitespace_visible(expected));
-        eprintln!("\nGOT:");
-        eprintln!("{}", make_whitespace_visible(result));
-        eprintln!("\n\nRaw strings:");
+        eprintln!("Diff between expected(-) and formatted output(+):");
+        let diff = TextDiff::from_lines(expected, result);
+        for change in diff.iter_all_changes() {
+            let text = make_whitespace_visible(&change.to_string());
+            match change.tag() {
+                ChangeTag::Insert => eprint!("\x1B[92m+{}\x1B[0m", text),
+                ChangeTag::Delete => eprint!("\x1B[91m-{}\x1B[0m", text),
+                ChangeTag::Equal => eprint!(" {}", text),
+            }
+        }
+        eprintln!("\nRaw strings:");
         eprintln!("\nEXPECTED (raw):");
         eprintln!("{:?}", expected);
         eprintln!("\nGOT (raw):");
@@ -28,37 +34,27 @@ fn assert_formatted_eq(result: &str, expected: &str, file_path: &Path) {
     }
 }
 
-#[test]
-fn test_all_sample_files() {
-    let input_dir = Path::new("tests/input");
-    let expected_dir = Path::new("tests/expected");
+test_each_file::test_each_path! { in "./tests/input" => test_file }
 
-    if !input_dir.exists() || !expected_dir.exists() {
-        panic!("Test directories do not exist");
-    }
+fn test_file(file_path: &Path) {
+    let file_name = file_path.file_name().expect("path is not a file path");
 
-    let input_files = fs::read_dir(input_dir)
-        .expect("Failed to read input directory")
-        .filter_map(|entry| entry.ok())
-        .filter(|entry| entry.path().extension().map_or(false, |ext| ext == "gd"))
-        .collect::<Vec<_>>();
+    let input_path = file_path;
+    let expected_path = file_path
+        .parent()
+        .unwrap()
+        .parent()
+        .unwrap()
+        .join("expected/")
+        .join(file_name);
 
-    for input_file in input_files {
-        let input_path = input_file.path();
-        let expected_path = expected_dir.join(input_file.file_name());
+    let input_content =
+        fs::read_to_string(&input_path).expect(&format!("Failed to read {}", input_path.display()));
+    let expected_content = fs::read_to_string(&expected_path)
+        .expect(&format!("Failed to read {}", expected_path.display()));
 
-        if !expected_path.exists() {
-            panic!("Expected file not found: {}", expected_path.display());
-        }
+    let result = format_gdscript(&input_content)
+        .expect(&format!("Failed to format {}", input_path.display()));
 
-        let input_content = fs::read_to_string(&input_path)
-            .expect(&format!("Failed to read {}", input_path.display()));
-        let expected_content = fs::read_to_string(&expected_path)
-            .expect(&format!("Failed to read {}", expected_path.display()));
-
-        let result = format_gdscript(&input_content)
-            .expect(&format!("Failed to format {}", input_path.display()));
-
-        assert_formatted_eq(&result, &expected_content, &input_path);
-    }
+    assert_formatted_eq(&result, &expected_content, &input_path);
 }
