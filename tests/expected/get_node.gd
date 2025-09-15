bf3568222720d820b286c77a9b0a24e8c4a3f163
Nathan Lovato <12694995+NathanLovato@users.noreply.github.com>
2025-09-15 12:54:59 +0200

    Add test cases for get_node syntaxes

---
+++ tests/expected/get_node.gd
@@ -0,0 +1,6 @@
+var test_1 = get_node("Node2D")
+var test_2 = get_node("Node2D/Sprite2D")
+var test_3 := $CanvasLayer/HealthBar
+var test_4: Sprite2D = %Sprite2D
+var test_4 = %AnimationPlayer
+var test_5 = $"/root/my node/Sprite2D"
