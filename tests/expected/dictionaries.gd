d6f520288cbeabc2a4cbe0384b23fba05e476e4c
Nathan Lovato <12694995+NathanLovato@users.noreply.github.com>
2025-09-19 13:12:33 +0200

    Add space after the opening brace and before closing braces of single line dictionaries
    Close #43

---
+++ tests/expected/dictionaries.gd
@@ -0,0 +1,11 @@
+# The output has long lines wrapped to 100 characters and trailing commas.
+var dialogue_items: Array[Dictionary] = [
+	{ "expression": expressions["regular"], "text": "I've been studying arrays and dictionaries lately.", "character": bodies["sophia"] },
+	{ "expression": expressions["regular"], "text": "Oh, nice. How has it been going?", "character": bodies["pink"] },
+]
+# Single line dict should have a space after { and before }
+var my_dictionary = { key = "value" }
+# But only if it fits on one line
+var my_dictionary_2 = {
+	key = "value"
+}
--- tests/input/dictionaries.gd
+++ tests/input/dictionaries.gd
@@ -3,3 +3,8 @@ var dialogue_items: Array[Dictionary] = [
 	{"expression"   : expressions["regular"],"text": "I've been studying arrays and dictionaries lately.","character": bodies["sophia"]},
 	{  "expression"  :  expressions[  "regular"] ,   "text": "Oh, nice. How has it been going?","character": bodies["pink"] },
 ]
+# Single line dict should have a space after { and before }
+var my_dictionary = {key = "value"}
+# But only if it fits on one line
+var my_dictionary_2 = {
+	key = "value"}
