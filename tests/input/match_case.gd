69c289cc587eaf920ab610459a82071cf56cac5d
Nathan Lovato <12694995+NathanLovato@users.noreply.github.com>
2025-09-23 10:29:08 +0200

    Add support for conditional expressions in match statements
    Close #33

--- tests/input/match_case.gd
+++ tests/input/match_case.gd
@@ -10,6 +10,11 @@ func foo():
 		_   :
 			pass
 	match 0:
+		# Warnings and comments should be preserved
 		@warning_ignore("unreachable_pattern")
 		0 when true:
 			print("true")
+	match 5:
+		# Conditional expression should be supported
+		0 if true else 2:
+			pass
