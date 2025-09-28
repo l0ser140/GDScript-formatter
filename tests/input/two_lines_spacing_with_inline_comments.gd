df936cde6d1f0925b81cacbaa02428727e877963
Nesterov Nikita <shadr.nn@gmail.com>
2025-09-28 18:03:03 +0300

    add tests for two line spacing with inline comments

---
+++ tests/input/two_lines_spacing_with_inline_comments.gd
@@ -0,0 +1,33 @@
+var a # This is a comment
+
+# comment
+
+# Ready
+func test():
+	pass
+
+# Ready
+func test():
+	pass
+
+var a # This is a comment
+
+# Ready
+func test():
+	pass
+
+
+var a # This is a comment
+# Ready
+func test():
+	pass
+
+var a # This is a comment
+func test():
+	pass
+
+var a # This is a comment
+# Two line
+# documentation
+func test():
+	pass
