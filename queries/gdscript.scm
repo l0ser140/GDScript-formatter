d860523da4c47bc07fe2842701eb4f365e5ce3bd
Nesterov Nikita <shadr.nn@gmail.com>
2025-09-28 16:53:27 +0300

    ensure only one empty line after extends_statement

--- queries/gdscript.scm
+++ queries/gdscript.scm
@@ -87,9 +87,9 @@
 
 (source
     (class_name_statement
-        extends: (extends_statement) @prepend_hardline @append_delimiter (#delimiter! "\n")) @append_hardline)
+        extends: (extends_statement) @append_delimiter @append_hardline (#delimiter! "\n")))
 (source
-    (extends_statement) @append_hardline @append_delimiter (#delimiter! "\n"))
+    (extends_statement) @append_delimiter @append_hardline (#delimiter! "\n"))
 (source (class_name_statement (name) @append_hardline))
 
 ; CONST DEFINITIONS
