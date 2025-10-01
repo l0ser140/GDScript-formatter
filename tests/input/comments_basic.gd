82de3125bdd5dd8d8f1b83253878e2805c4749cd
Nate Moore <nate@moore.codes>
2025-10-01 00:36:19 -0700

    Update tree-sitter-gdscript and add trailing comments tests

--- tests/input/comments_basic.gd
+++ tests/input/comments_basic.gd
@@ -11,3 +11,17 @@ var a = 10 # inline comment
 
 var b = 10
 # after statement comment
+
+func do_thing() -> void:
+	if true:
+		# this is a comment inside of the function block
+		pass
+		
+		# this is a comment at the end of the function block
+		# it should stay inside of this function block
+
+
+func do_another_thing() -> void:
+	pass
+
+	# likewise, this comment should stay inside of this function block
