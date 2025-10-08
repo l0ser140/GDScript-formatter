4b71390d76eb5a1d937de364e5e4cac11ccc1f1f
Christian Höltje <docwhat@gerf.org>
2025-10-08 15:18:31 -0400

    add test for trailing whitespace

---
+++ tests/expected/trailing_spaces.gd
@@ -0,0 +1,11 @@
+## This function is the best in the world.
+##
+## There are no mistakes in it.
+func best_function() -> String:
+	var new_string: String = """
+	This is a multi-line string with trailing spaces.  
+	"""
+
+	return new_string
+
+# EOF
